c122c113a3cced740e31b4fe2955596b66ea40f5
aantunes <ana.antunes@premium-minds.com>
2020-05-29 13:58:51 +0100

    css

--- src/app.js
+++ src/app.js
@@ -1,22 +1,27 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import { MainViewConnected } from "./components/Main";
 import { Layout} from 'antd';
 import { CheckoutConnected } from './components/Checkout';
-const { Header, Footer, Content } = Layout;
+const { Header, Content } = Layout;
 
 export const AppLayout = (componentToRender) => {
   const Component = componentToRender;
   return () => (
-    <Layout className="layout">
-      <Header className="navbar theme-content-light navbar-expand"> 
+    <Fragment>
+      <Header className="theme-content-light">
         <h2 className="p-10-20 d-md-block d-none"><Link to="/">Store App</Link></h2>
       </Header>
-      <Content className="site-layout-content">
-          <Component />
-      </Content> 
-      <Footer className="theme-content-light"/> 
-    </Layout>
+      <Layout>
+        <Content className="site-layout-content content p-md-5 pt-5 p-3">
+          <div className="row p-40-0">
+            <div className="col-12 max-content-width">
+              <Component />
+            </div>
+          </div>
+        </Content>
+      </Layout>
+    </Fragment>
   );
 }
   

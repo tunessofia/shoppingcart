810b14a390475694f9503469eb9eb9f2f63e4c3e
aantunes <ana.antunes@premium-minds.com>
2020-11-11 15:32:37 +0000

    fix test

--- frontend/src/tests/reducers/cart.reducer.test.js
+++ frontend/src/tests/reducers/cart.reducer.test.js
@@ -1,12 +1,12 @@
 import shoppingCart from '../../reducers/cart.js';
-import { ADD_ITEM, REMOVE_ITEM, CHANGE_ITEM_QUANTITY } from '../../actions/cart';
+import { ADD_ITEM } from '../../actions/cart';
 
 describe('cart reducer test', () => {
     it('should handle a first add item to cart', () => {
         const action = { type: ADD_ITEM, payload: { itemId: 1 } } ;
 
         const expectedResult = [{id: 1, quantity: 1 }];
-debugger;
+
         expect(shoppingCart([], action))
         .toEqual(expectedResult);
     });
